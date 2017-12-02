1fc75ffddbed3bb7d84b5319a4218fe1457de57a
Fabio Tiriticco <fabio.europe@gmail.com>
2017-12-02 13:53:09 +0100

    notes as per the approach to follow

--- src/main/scala/io/ticofab/akkaclusterkubernetes/AkkaClusterKubernetesApp.scala
+++ src/main/scala/io/ticofab/akkaclusterkubernetes/AkkaClusterKubernetesApp.scala
@@ -16,8 +16,27 @@ object AkkaClusterKubernetesApp extends App {
     as.actorOf(Props(new InputSource(master)), "inputSource")
   }
 
-  // https://hackernoon.com/akka-streams-a-story-of-scalability-5d9e7c2d3ac3
-  // https://doc.akka.io/docs/akka/2.5.7/distributed-pub-sub.html?language=scala
-  // http://letitcrash.com/post/29044669086/balancing-workload-across-nodes-with-akka-2
+
+  /*
+    approaches:
+
+    APP level:
+
+      1. akka streams + mapAsync (needs to have the number of worker nodes)
+      2. raw actors (distributed pub/sub? mediator? pull work)
+
+            https://hackernoon.com/akka-streams-a-story-of-scalability-5d9e7c2d3ac3
+            https://doc.akka.io/docs/akka/2.5.7/distributed-pub-sub.html?language=scala
+            http://letitcrash.com/post/29044669086/balancing-workload-across-nodes-with-akka-2
+
+
+    INFRA level:
+
+      1. using yaml files and deploying and API service to spin up new nodes as needed
+      2. introduce an operator to manage the cluster more independently
+
+
+   */
+
 
 }
