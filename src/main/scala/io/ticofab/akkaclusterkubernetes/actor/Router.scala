30b403935eb5e9d52dd836054217c35716a12a51
Fabio Tiriticco <fabio.europe@gmail.com>
2018-03-30 09:19:55 +0200

    rate evaluation

--- src/main/scala/io/ticofab/akkaclusterkubernetes/actor/Router.scala
+++ src/main/scala/io/ticofab/akkaclusterkubernetes/actor/Router.scala
@@ -1,5 +1,7 @@
 package io.ticofab.akkaclusterkubernetes.actor
 
+import java.time.LocalDateTime
+
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.cluster.Cluster
 import akka.cluster.ClusterEvent.{MemberEvent, MemberUp, UnreachableMember}
@@ -7,7 +9,7 @@ import akka.cluster.routing.{ClusterRouterGroup, ClusterRouterGroupSettings}
 import akka.routing.RoundRobinGroup
 import akka.stream.ActorMaterializer
 import io.ticofab.akkaclusterkubernetes.actor.InputSource.Job
-import io.ticofab.akkaclusterkubernetes.actor.Router.JobCompleted
+import io.ticofab.akkaclusterkubernetes.actor.Router.{EvaluateRate, JobCompleted}
 
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.duration._
@@ -33,14 +35,21 @@ class Router(scalingController: ActorRef) extends Actor with ActorLogging {
     name = "workerRouter")
 
   // the queue of jobs to run
-  val jobs = ListBuffer.empty[Job]
+  val waitingJobs = ListBuffer.empty[Job]
+  val submittedJobs = ListBuffer.empty[Job]
+  val completedJobs = ListBuffer.empty[(JobCompleted, LocalDateTime)]
 
   def submitNextJob(): Unit =
-    jobs.headOption.foreach(
+    waitingJobs.headOption.foreach(
       job => {
         workersPool ! job
-        jobs -= job
+        waitingJobs -= job
+        submittedJobs += job
       })
+
+  val rateInterval = 10.seconds
+  context.system.scheduler.schedule(rateInterval, rateInterval, self, EvaluateRate)
+
 
   override def receive = {
 
@@ -52,13 +61,41 @@ class Router(scalingController: ActorRef) extends Actor with ActorLogging {
     case job: Job =>
       // enqueue the new job
       log.debug("received a new job: {}", job.number)
-      jobs += job
+      waitingJobs += job
 
     case jobCompleted: JobCompleted =>
       // a job has been completed: trigger the next one
       log.debug("job {} completed", jobCompleted.number)
+      completedJobs += ((jobCompleted, LocalDateTime.now))
       submitNextJob()
 
+    case EvaluateRate =>
+      val rate = {
+        val recentWaitingJobs = waitingJobs filter { job => job.creationDate.isAfter(LocalDateTime.now minusSeconds 10) }
+        val recentSubmittedJobs = submittedJobs filter { job => job.creationDate.isAfter(LocalDateTime.now minusSeconds 10) }
+        val recentCompletions = completedJobs filter { case (jc, ts) => ts.isAfter(LocalDateTime.now minusSeconds 10) }
+        val rate = recentCompletions.size.toDouble / (recentWaitingJobs.size.toDouble + recentSubmittedJobs.size.toDouble)
+        log.debug("in the last 10 seconds, we received {} jobs and completed {}. Rate is {}",
+          recentWaitingJobs.size + recentSubmittedJobs.size, recentCompletions.size, rate)
+        rate
+      }
+
+      // we need rate to be slightly above 1.0, say between 1.0 and 1.2
+      if (rate < 1.0) {
+        log.debug("adding node")
+        // there are more incoming jobs than processing speed ---> add node
+        // scalingController ! AddNode
+      } else if (rate > 1.2) {
+        log.debug("removing node")
+        // there is too much processing power ---> remove node
+        // scalingController ! RemoveNode
+      }
+
+
+      // TODO:
+      // if there are workers to consume all incoming messages, the rate will be 1.0. But we still have way too many
+      // workers than necessary. So maybe the threshold to scale up is if we are below 1.2 and scale up if b
+      // TODO: just think about the rate taking all cases into consideration
   }
 }
 
@@ -66,5 +103,7 @@ object Router {
   def apply(scalingController: ActorRef): Props = Props(new Router(scalingController))
 
   case class JobCompleted(number: Int)
+
+  case object EvaluateRate
 
 }
